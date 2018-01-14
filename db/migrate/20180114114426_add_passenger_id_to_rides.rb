ef6de3aa0f56b25a4fddec9a84be57262255e986
Robert Laws <roblaws@me.com>
2018-01-14 11:44:26 +0000

    automatically backed up by learn

---
+++ db/migrate/20180114114426_add_passenger_id_to_rides.rb
@@ -0,0 +1,7 @@
+class AddPassengerIdToRides < ActiveRecord::Migration
+  def change
+    add_column :rides, :, :rides,
+    add_column :rides, :, :passenger_id,
+    add_column :rides, :, :integer
+  end
+end
